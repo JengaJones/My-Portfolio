21d04a51bbdb690ed7aacae2de52dff4df81c981
Jenga Jones <rocknroll2793@gmail.com>
2024-11-24 19:08:34 -0600

    Establishing Git

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <h1>Welcome to My Portfolio</h1>
         <nav>
           <ul>
-            <li><a href="#about">About Me</a></li>
+            <li><a href="#about">Not About Me</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
